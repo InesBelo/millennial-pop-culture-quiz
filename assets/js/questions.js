14bcc7e49ef6ebe697b98e37817534b90d4eaddf
InesBelo <>
2023-06-15 12:53:01 +0000

    Add first thirty questions to the quiz

---
+++ assets/js/questions.js
@@ -0,0 +1,344 @@
+const questions = [
+    {
+        numb: 1,
+        question: "What is the name of the group responsible for the hit song 'Macarena'?",
+        answer: "Los del Río",
+        options: [
+            "Los del Aire",
+            "Los del Río",
+            "Los de la Noche",
+            "Los del Mar"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Whitney Houston had a massive hit in 1992 with a cover of whose hit?",
+        answer: "'I Will Always Love You', by Dolly Parton",
+        options: [
+            "'Common People', by Pulp",
+            "'I Will Always Love You', by Dolly Parton",
+            "'Waterfalls', by TLC",
+            "'Big Time Sensuality', by Björk"
+        ]
+    },
+    {
+        numb: 3,
+        question: "What's the infamous haircut name inspired by a 'Must See TV' sitcom from the 90's?",
+        answer: "'The Rachel', from Jennifer Anniston carachter on 'Friends'",
+        options: [
+            "'The Xena', from Lucy Lawless character on 'Xena: the Warrior Princess'",
+            "'The Buffy', from Sarah Michelle Gellar character on 'Buffy: the Vampire Slayer '",
+            "'The Scully', from Gillian Anderson character on 'The X-Files'",
+            "'The Rachel', from Jennifer Anniston character on 'Friends'"
+        ]
+    },
+    {
+        numb: 4,
+        question: "What was the first animated feature film ever to be nominated for a Best Picture Oscar?",
+        answer: "Walt Disney Studios 'Beauty and the Beast'(1991)",
+        options: [
+            "Studio Ghibli 'Princess Mononoke'(1997)",
+            "Walt Disney Studios 'Beauty and the Beast'(1991)",
+            "Nest Entertainment and Rich Animation Studios 'Swan Princess'(1994)",
+            "Warner Bros 'Thumbelina'(1994)"
+        ]
+    },
+    {
+        numb: 5,
+        question: "The child actor Macaulay Culkin was paid 1$ Million for starring in what movie?",
+         answer: "'My Girl'(1991)",
+        options: [
+            "'Home Alone'(1990)",
+            "'My Girl'(1991)",
+            "'The Good Son' (1993)",
+             "'Uncle Buck'(1989)"
+        ]
+     },
+     {
+        numb: 6,
+        question: "What famous fashion accessory was invented in the 90's by a high school teacher?",
+        answer: "Slap bracelets",
+        options: [
+            "Toe rings",
+            "Scrunchies",
+            "Bandanas",
+            "Slap bracelets"
+        ]
+    },
+    {
+        numb: 7,
+        question: "Power Rangers was banned in what country until 2011?",
+        answer: "New Zealand",
+        options: [
+            "New Zealand",
+            "Australia",
+            "Fiji",
+            "Brazil"
+        ]
+    },
+    {
+        numb: 8,
+        question: "What video game was the first ever to be played in space?",
+        answer: "Tetris",
+        options: [
+            "'Starcraft",
+            "'Tetris'",
+            "Planescape: Torment",
+            "Homeworld"
+        ]
+    },
+    {
+        numb: 9,
+        question: "What was the first rap song to hit No.1 on the Billboard Hot 100?",
+        answer: "'Ice Ice Baby' by Vanilla Ice (1990)",
+        options: [
+            "'I Used to Love H.E.R.' by Common (1994)",
+            "'N.Y. State of Mind' by Nas (1994)",
+            "'Hit 'Em Up' by Tupac Shakur (1996)",
+            "'Ice Ice Baby' by Vanilla Ice (1990)"
+        ]
+    },
+    {
+        numb: 10,
+        question: "What was the bestselling video game console of the 90's?",
+        answer: "Sony Playstation",
+        options: [
+            "Sony Playstation",
+            "Sega Saturn",
+            "Nintendo 64",
+            "Game Boy Color"
+        ]
+    },
+    {
+        numb: 11,
+        question: "What music band is featurend on Beavis shirt (from the show 'Beavis and Butt-Head')?",
+        answer: "Metallica",
+        options: [
+            "Pearl Jam",
+            "Metallica",
+            "Red Hot Chilli Peppers",
+            "Nirvana"
+        ]
+    },
+    {
+        numb: 12,
+        question: "'Tamagotchi' is a combination of two japanese words that mean what?",
+        answer: "'Egg' and 'friend'",
+        options: [
+            "'Egg' and 'friend'",
+            "'Cat' and 'dog'",
+            "'Friend' and 'cat'",
+            "'Egg' and 'dog'"
+        ]
+    },
+    {
+        numb: 13,
+        question: "When was the 'World Wide Web' first introduced?",
+        answer: "1991",
+        options: [
+            "1988",
+            "1989",
+            "1990",
+            "1991"
+        ]
+    },
+    {
+        numb: 14,
+        question: "What was the bestselling Rock album of the 90's?",
+        answer: "Alanis Morissette's 'Jagged Little Pill' (1995)",
+        options: [
+            "Nirvana's 'Nevermind' (1991)",
+            "Green Day's 'Dookie' (1994)",
+            "Alanis Morissette's 'Jagged Little Pill' (1995)",
+            "No Doubt's 'Tragic Kingdom' (1995)"
+        ]
+    },
+    {
+        numb: 14,
+        question: "When did the Google search engine first debut?",
+        answer: "1998",
+        options: [
+            "1995",
+            "1996",
+            "1997",
+            "1998"
+        ]
+    },
+    {
+        numb: 15,
+        question: "Which Spice Girls singer wasn't an original member of the group?",
+        answer: "Emma Button ('Baby Spice')",
+        options: [
+            "Victoria Beckam (née Adams, 'Posh Spice')",
+            "Emma Button ('Baby Spice')",
+            "Melanie Brown ('Scary Spice')",
+            "Geri Halliwell ('Ginger Spice')"
+        ]
+    },
+    {
+        numb: 16,
+        question: "What does VHS stand for?",
+        answer: "Video Home System",
+        options: [
+            "Video Home System",
+            "Video House System",
+            "Videomapping House System",
+            "Variety Home System"
+        ]
+    },
+    {
+        numb: 17,
+        question: "Which video game was the most popular video game of the 80's?",
+        answer: "Mario Bros.",
+        options: [
+            "Mario Bros.",
+            "Donkey Kong",
+            "Pac-Man",
+            "Robotron 2084"
+        ]
+    },
+    {
+        numb: 18,
+        question: "Which computer sold in the 80's still holds the Guinness World Record for the highest-selling computer of all time?",
+        answer: "The Commodore 64",
+        options: [
+            "VIC-20",
+            "The Commodore 64",
+            "Apple II",
+            "Atari 400/800"
+        ]
+    },
+    {
+        numb: 19,
+        question: "What year was the Berlin Wall torn down?",
+        answer: "1989",
+        options: [
+            "1988",
+            "1989",
+            "1990",
+            "1991"
+        ]
+    },
+    {
+        numb: 20,
+        question: "Which advertising slogan was launched by Nike in 1988?",
+        answer: "'Just do it'",
+        options: [
+            "'The quicker picker upper",
+            "'Just do it",
+            "'The original. If your grandfather hadn't worn it, you wouldn't exist'",
+            "Let's go places"
+        ]
+    },
+    {
+        numb: 21,
+        question: "What was the name of the space shuttle that tragically exploded in 1986?",
+        answer: "'Challenger'",
+        options: [
+            "'Challenger'",
+            "'Commander'",
+            "'Defender'",
+            "Surrender'"
+        ]
+    },
+    {
+        numb: 22,
+        question: "Which shoe had a resurgence in the 80's?",
+        answer: "Converse Chuck Taylor All-Star",
+        options: [
+            "Converse All-Stars",
+            "Doc Martens",
+            "Air Jordans",
+            "Adidas Superstar"
+        ]
+    },
+    {
+        numb: 23,
+        question: "Who was synonymous with workout videos during the 80's?",
+        answer: "Jane Fonda",
+        options: [
+            "Jamie Lee Curtis",
+            "Olivia Newton-John",
+            "Lily Tomlin",
+            "Jane Fonda"
+        ]
+    },
+    {
+        numb: 24,
+        question: "What is the name of the iconic hairstyle that was short at the front and sides, but long at the back?",
+        answer: "The Mullet",
+        options: [
+            "The Shag",
+            "The Wedge",
+            "The Mullet",
+            "The Feathered Flip"
+        ]
+    },
+    {
+        numb: 25,
+        question: "What sort of car is used as a time machine in 'Back to the Future' (1985)?",
+        answer: "DeLorean DMC-12",
+        options: [
+            "Ferrari Testarrossa",
+            "DeLorean DMC-12",
+            "Datsun 240Z",
+            "Pontiac Firebird"
+        ]
+    },
+    {
+        numb: 26,
+        question: "Which confectionary product was made famous by E.T.?",
+        answer: "Reese's Pieces",
+        options: [
+            "Penny Sweets",
+            "Opal Fruits",
+            "Reese's Pieces",
+            "Fizzle Sticks"
+        ]
+    },
+    {
+        numb: 27,
+        question: "What is the name of the iconic cartoon that started as a series of short skits on the Tracey Ullman Show in 1987?",
+        answer: "'The Simpsons'",
+        options: [
+            "'Futurama'",
+            "'King of the Hill'",
+            "'South Park'",
+            "'The Simpsons'"
+        ]
+    },
+    {
+        numb: 28,
+        question: "'Buffy' was the original name for which iconic Star Wars character?",
+        answer: "Yoda",
+        options: [
+            "Obi-Wan Kenobi",
+            "Ahsoka Tano",
+            "Jabba the Hut",
+            "Boba Fett"
+        ]
+    },
+    {
+        numb: 29,
+        question: "What is the name of the evil wizard who featured in the animated show He-Man?",
+        answer: "Skeletor",
+        options: [
+            "Gargamel",
+            "Skeletor",
+            "Medivh",
+            "Randal Flagg"
+        ]
+    },
+    {
+        numb: 30,
+        question: "What is the name of the van that Scooby Doo and his friends travel around in?",
+        answer: "The Mistery Machine",
+        options: [
+            "The Secret Ride",
+            "The Fast Car",
+            "The Phantom Heap",
+            "The Mistery Machine"
+        ]
+    },
+    
+    ]
